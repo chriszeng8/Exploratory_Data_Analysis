b5054ecc7ad388038a19b975e5ace14ef87e6f06
chriszeng8 <chriszeng8@hotmail.com>
2015-03-20 21:53:10 -0400

    corrplot packages

---
+++ corrplot/Corr_Plot.R
@@ -0,0 +1,30 @@
+#Source:
+#http://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
+#
+library(corrplot)
+M <- cor(mtcars)
+corrplot(M, method = "circle")
+# I personally think this is one of the most useful plot.
+corrplot.mixed(M)
+
+cor.mtest <- function(mat, conf.level = 0.95) {
+     mat <- as.matrix(mat)
+     n <- ncol(mat)
+     p.mat <- lowCI.mat <- uppCI.mat <- matrix(NA, n, n)
+     diag(p.mat) <- 0
+     diag(lowCI.mat) <- diag(uppCI.mat) <- 1
+     for (i in 1:(n - 1)) {
+          for (j in (i + 1):n) {
+               tmp <- cor.test(mat[, i], mat[, j], conf.level = conf.level)
+               p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+               lowCI.mat[i, j] <- lowCI.mat[j, i] <- tmp$conf.int[1]
+               uppCI.mat[i, j] <- uppCI.mat[j, i] <- tmp$conf.int[2]
+          }
+     }
+     return(list(p.mat, lowCI.mat, uppCI.mat))
+}
+
+res1 <- cor.mtest(mtcars, 0.95)
+res2 <- cor.mtest(mtcars, 0.99)
+## specialized the insignificant value according to the significant level
+corrplot(M, p.mat = res1[[1]], sig.level = 0.2)
